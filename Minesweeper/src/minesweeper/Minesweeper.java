3e5a5085f19a78e0c71d4c4aa624ba18f2aab529
ITITIU21299 <matofottv@gmail.com>
2023-12-31 22:03:25 +0700

    Add background music

--- Minesweeper/src/minesweeper/Minesweeper.java
+++ Minesweeper/src/minesweeper/Minesweeper.java
@@ -4,6 +4,11 @@
  */
 package minesweeper;
 
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
 /**
  *
  * @author nguye
@@ -17,6 +22,26 @@ public class Minesweeper {
         // TODO code application logic here
         Menu menu = new Menu();
         menu.setVisible(true);
+        
+        String filepath = "src\\sound\\Schubert - Serenade.wav";
+        PlayMusic(filepath);
+        
     }
     
+    public static void PlayMusic(String location){
+        try {
+            File musicPath = new File(location);
+            if (musicPath.exists()){
+                AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioInput);
+                clip.start();
+            }
+            else{
+                System.out.println("Can't find file");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
