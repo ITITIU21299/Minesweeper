9fa8974346e20debbaa0dfed59430543d21aeb31
ITCSIU21009 <118718862+ITCSIU21009@users.noreply.github.com>
2024-01-01 20:57:55 +0700

    Update game logic

--- Minesweeper/src/minesweeper/Minesweeper.java
+++ Minesweeper/src/minesweeper/Minesweeper.java
@@ -8,6 +8,11 @@ import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+import java.util.ArrayList;
 
 /**
  *
@@ -18,6 +23,173 @@ public class Minesweeper {
     /**
      * @param args the command line arguments
      */
+    private class MineTile extends JButton {
+        int rows;
+        int cols;
+
+        public MineTile(int rows, int cols) {
+            this.rows = rows;
+            this.cols = cols;
+        }
+    }
+
+    int tileSize = 70;
+    int numRows = 8;
+    int numCols = 8;
+    int boardWidth = numCols * tileSize;
+    int boardHeight = numRows * tileSize;
+
+    JFrame frame = new JFrame("Minesweeper");
+    JLabel textLabel = new JLabel();
+    JPanel textPanel = new JPanel();
+    JPanel boardPanel = new JPanel();
+
+    int numberOfMines = Math.round((10 * numRows * numCols) / 100.0f);
+    MineTile[][] board = new MineTile[numRows][numCols];
+    ArrayList<MineTile> mineList;
+    Random random = new Random();
+
+    int tilesClicked = 0;
+    boolean gameOver = false;
+
+    public Minesweeper() {
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        textLabel.setFont(new Font("Arial", Font.BOLD, 25));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setText("Number of mines: " + Integer.toString(numberOfMines));
+        textLabel.setOpaque(true);
+
+        textPanel.setLayout(new BorderLayout());
+        textPanel.add(textLabel);
+        frame.add(textPanel, BorderLayout.NORTH);
+
+        boardPanel.setLayout(new GridLayout(numRows, numCols));
+        frame.add(boardPanel);
+
+        for (int rows = 0; rows < numRows; rows++) {
+            for (int cols = 0; cols < numCols; cols++) {
+                MineTile tile = new MineTile(rows, cols);
+                board[rows][cols] = tile;
+
+                tile.setFocusable(false);
+                tile.setMargin(new Insets(0, 0, 0, 0));
+                tile.setFont(new Font("Arial Unicode MS", Font.PLAIN, 45));
+                tile.addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mousePressed(MouseEvent e) {
+                        if (gameOver) {
+                            return;
+                        }
+                        MineTile tile = (MineTile) e.getSource();
+
+                        if (e.getButton() == MouseEvent.BUTTON1) {
+                            if (tile.getText() == "") {
+                                if (mineList.contains(tile)) {
+                                    revealMine();
+                                } else {
+                                    checkMine(tile.rows, tile.cols);
+                                }
+                            }
+                        } else if (e.getButton() == MouseEvent.BUTTON3) {
+                            if (tile.getText() == "" && tile.isEnabled()) {
+                                tile.setText("🚩");
+                            } else if (tile.getText() == "🚩") {
+                                tile.setText("");
+                            }
+                        }
+                    }
+                });
+                boardPanel.add(tile);
+            }
+        }
+        frame.setVisible(true);
+
+        generateMines();
+    }
+
+    public void generateMines() {
+        mineList = new ArrayList<MineTile>();
+        int mineLeft = numberOfMines;
+        while (mineLeft > 0) {
+            int r = random.nextInt(numRows);
+            int c = random.nextInt(numCols);
+
+            MineTile tile = board[r][c];
+            if (!mineList.contains(tile)) {
+                mineList.add(tile);
+                mineLeft--;
+            }
+        }
+    }
+
+    public void revealMine() {
+        for (int i = 0; i < mineList.size(); i++) {
+            MineTile tile = mineList.get(i);
+            tile.setText("💣");
+        }
+        gameOver = true;
+        textLabel.setText("Game Over!");
+    }
+
+    public void checkMine(int r, int c) {
+        if (r < 0 || r >= numRows || c < 0 || c >= numCols) {
+            return;
+        }
+
+        MineTile tile = board[r][c];
+        if (!tile.isEnabled()) {
+            return;
+        }
+        tile.setEnabled(false);
+        tilesClicked++;
+
+        int minesFound = 0;
+
+        minesFound += countMine(r - 1, c - 1);
+        minesFound += countMine(r - 1, c);
+        minesFound += countMine(r - 1, c + 1);
+        minesFound += countMine(r, c - 1);
+        minesFound += countMine(r, c + 1);
+        minesFound += countMine(r + 1, c - 1);
+        minesFound += countMine(r + 1, c);
+        minesFound += countMine(r + 1, c + 1);
+
+        if (minesFound > 0) {
+            tile.setText(Integer.toString(minesFound));
+        } else {
+            tile.setText("");
+
+            checkMine(r - 1, c - 1);
+            checkMine(r - 1, c);
+            checkMine(r - 1, c + 1);
+            checkMine(r, c - 1);
+            checkMine(r, c + 1);
+            checkMine(r + 1, c - 1);
+            checkMine(r + 1, c);
+            checkMine(r + 1, c + 1);
+        }
+
+        if (tilesClicked == numRows * numCols - mineList.size()) {
+            gameOver = true;
+            textLabel.setText("Mines Cleared, You Win!");
+        }
+    }
+
+    public int countMine(int r, int c) {
+        if (r < 0 || r >= numRows || c < 0 || c >= numCols) {
+            return 0;
+        }
+        if (mineList.contains(board[r][c])) {
+            return 1;
+        }
+        return 0;
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
         Menu menu = new Menu();
